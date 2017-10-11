539e2dce6a6da6c341392514a8885e023a992c34
jayanthi1212 <bhumijayareddy27@gmail.com>
2017-10-11 20:30:18 +0530

    Initial commit

---
+++ app/src/main/java/com/ench/jaya/tictakkkkkkkk/Win.java
@@ -0,0 +1,36 @@
+package com.ench.jaya.tictakkkkkkkk;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Win extends Activity{
+    TextView winn;
+    Button b;
+    final Context context=this;
+    ImageView im;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.win_layout);
+        Intent intent = getIntent();
+        String win=intent.getStringExtra("winner");
+        winn=(TextView)findViewById(R.id.wintxt);
+        b=(Button)findViewById(R.id.back);
+
+        winn.setText(win);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(context,MainActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
+
